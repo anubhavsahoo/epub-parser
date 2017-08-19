e23097b08a038427dfc36970ce2fd8e6ef23f6fa
anubhav.sahoo <anubhav.sahoo@gmail.com>
2017-08-19 21:50:24 +0530

    ver 0.3.3 - added opf meta editing

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-import Epub from './dist/epub-parser.js';
+// import Epub from './dist/epub-parser.js';
+import Epub from './lib/epub-parser.js';
 
 // EP.open('./example/testbook.epub')
 // .then(console.log).catch(console.error);
@@ -6,8 +7,12 @@ import Epub from './dist/epub-parser.js';
 const ep = new Epub('./example/testbook.epub');
 ep.open().then((ep)=>{
   try{
-    console.log(ep);
-    console.log(ep.getMetadata());
+    // console.log(ep);
+    // console.log(ep.getMetadata());
+    console.log(ep._opf['metadata']);
+
+    ep.setOpfMeta('dc:say', 'friendster999').then(console.log).catch(console.error);
+
   } catch(e) {
     console.error(e);
     throw e;
